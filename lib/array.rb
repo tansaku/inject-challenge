2f558b092ccf1f76311c1a7e582b642c38c96d00
Sam Joseph <tansaku@gmail.com>
2015-03-27 14:38:36 +0000

    shorter method

--- lib/array.rb
+++ lib/array.rb
@@ -1,12 +1,8 @@
 class Array
   def inject_clone *args
-    if args.empty? || args[0].is_a?(Symbol)
-      memo = self[0]
-      clone = self.drop(1)
-    else
-      memo = args[0]
-      clone = self
-    end
+    memo = no_args_or_symbol?(args) ? self[0] : args[0]
+    clone = no_args_or_symbol?(args) ? self.drop(1) : self
+
     if args.index_of_symbol
       clone.each { |e| memo = memo.send(args[args.index_of_symbol], e) }
     else
@@ -17,5 +13,9 @@ class Array
 
   def index_of_symbol
     self.map { |i| i.is_a? Symbol }.index(true)
+  end
+
+  def no_args_or_symbol? args
+    args.empty? || args[0].is_a?(Symbol)
   end
 end
