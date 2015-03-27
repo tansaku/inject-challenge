399c139d5370ff7d88bb73ab861c94580756fe4a
Sam Joseph <tansaku@gmail.com>
2015-03-27 13:41:10 +0000

    specific use of blocks

--- spec/array_spec.rb
+++ spec/array_spec.rb
@@ -3,11 +3,16 @@ require 'array'
 describe Array do
   subject { [1, 2, 3] }
   it 'sums numbers via inject' do
-    operation = -> (memo, element) { memo += element }
-    expect(subject.inject_clone(&operation)).to eq subject.inject(&operation)
+    original = subject.inject { |memo, element| memo + element }
+    result = subject.inject_clone { |memo, element| memo + element }
+    expect(result).to eq original
   end
-  it 'subracts numbers via inject' do
-    operation = -> (memo, element) { memo -= element }
-    expect(subject.inject_clone(&operation)).to eq subject.inject(&operation)
+  it 'subtracts numbers via inject' do
+    original = subject.inject { |memo, element| memo - element }
+    result = subject.inject_clone { |memo, element| memo - element }
+    expect(result).to eq original
   end
+  # it 'sums numbers with a base' do
+  #   expect(subject.inject_clone(&operation)).to eq subject.inject(&operation)
+  # end
 end
