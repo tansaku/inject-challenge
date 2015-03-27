1188f2987f7e269096081a25669544a87fc60831
Sam Joseph <tansaku@gmail.com>
2015-03-27 16:10:38 +0000

    weird 4 class version

--- lib/array.rb
+++ lib/array.rb
@@ -1,21 +1,12 @@
-class Array
-  def inject_clone *args
-    memo = no_args_or_symbol?(args) ? self[0] : args[0]
-    clone = no_args_or_symbol?(args) ? self.drop(1) : self
+require_relative 'injector_factory'
 
-    if args.index_of_symbol
-      clone.each { |e| memo = memo.send(args[args.index_of_symbol], e) }
-    else
-      clone.each { |e| memo = yield memo, e }
-    end
-    memo
+class Array
+  def inject_clone *args, &block
+    injector = InjectorFactory.create(self, args, block)
+    injector.memo
   end
 
   def index_of_symbol
     self.map { |i| i.is_a? Symbol }.index(true)
-  end
-
-  def no_args_or_symbol? args
-    args.empty? || args[0].is_a?(Symbol)
   end
 end
